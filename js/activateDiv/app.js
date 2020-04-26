0bcaba6ae19a38aa28b9d33bcb30174e670c3598
Hubert Sikorski <sikorski.sr@gmail.com>
2020-04-26 14:29:09 +0200

    add vanila js activateDiv

---
+++ js/activateDiv/app.js
@@ -0,0 +1,20 @@
+const divs = ['Div1', 'Div2', 'Div3', 'Div4', 'Div5'];
+const parentDiv = document.querySelector('.container');
+
+const renderDivs = () => {
+  divs.map(div => {
+    const childDiv = document.createElement('div');
+    parentDiv.appendChild(childDiv);
+    childDiv.className = 'child';
+    childDiv.innerHTML = div;
+    childDiv.addEventListener('click', () => {
+      if (childDiv.className === 'child') {
+        childDiv.className = 'child isActive';
+        childDiv.innerHTML = `${div} activated`;
+      } else {
+        childDiv.className = 'child';
+        childDiv.innerHTML = div;
+      }
+    });
+  });
+};
