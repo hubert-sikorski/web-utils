7ca8a24f20bc60b8dfcd030a79363fab9f3371d3
Hubert Sikorski <sikorski.sr@gmail.com>
2020-04-26 13:20:22 +0200

    add disabledocumentscroll

---
+++ js/disable-document-scroll.js
@@ -0,0 +1,5 @@
+// useful for mobile menus
+
+const disableAppScroll = () => {
+  document.documentElement.style = 'overflow: hidden;';
+};
--- js/get-user-language.js
+++ js/get-user-language.js
@@ -1,15 +1,17 @@
-getUserLang() {
+const getUserLang = () => {
   let lang = window.navigator.language.slice(0, 2);
-  if (lang === 'en' ||
-      (lang !== 'en' &&
-       lang !== 'fr' &&
-       lang !== 'de' &&
-       lang !== 'nl' &&
-       lang !== 'pl')) {
+  if (
+    lang === 'en' ||
+    (lang !== 'en' &&
+      lang !== 'fr' &&
+      lang !== 'de' &&
+      lang !== 'nl' &&
+      lang !== 'pl')
+  ) {
     lang = 'gb';
   }
   // if (config.translationsCompleted) {
   //   this.$i18n.locale = lang;
   // }
   console.log(lang);
-}
+};
