c77ec3fc441a9a471eabe1745c4e2978d19f8a86
Hubert Sikorski <sikorski.sr@gmail.com>
2020-06-14 19:27:48 +0200

    add base64 file uploader

---
+++ js/base64FileUploader/app.js
@@ -0,0 +1,28 @@
+const handleFileChange = elementID => {
+  const selectedFile = document.getElementById(elementID).files;
+  const fileReader = new FileReader();
+  const regex = /data:application\/pdf;base64,/;
+  let fileToLoad;
+  let base64;
+
+  if (selectedFile.length) fileToLoad = selectedFile[0];
+
+  fileReader.readAsDataURL(fileToLoad);
+  fileReader.onload = fileLoadedEvent => {
+    base64 = fileLoadedEvent.target.result;
+    const dataToPass = base64.replace(regex, '');
+    console.log(dataToPass);
+  };
+};
+
+const displayFileName = (querySelector, elementID) => {
+  document.querySelector(querySelector).textContent = document.getElementById(
+    elementID
+  ).files[0].name;
+};
+
+// const clearFile = () => {
+//   document.getElementById('fileUploader').value = '';
+//   document.querySelector('.file-name').textContent = this.$t('upload');
+//   this.file = '';
+// };
